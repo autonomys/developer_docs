dbd3d45baba563c75755d45dc028adbd284d496b
Emil Fattakhov <emil.e.fattakhov@gmail.com>
2025-01-15 15:15:52 -0500

    Undo some unnecessary changes

--- next.config.mjs
+++ next.config.mjs
@@ -9,30 +9,4 @@ const withNextra = nextra({
   }
 })
 
-// Configuration object with headers
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*'
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT'
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-          }
-        ]
-      }
-    ]
-  }
-}
-
-// Combine Nextra with the custom config
-export default withNextra(nextConfig)
+export default withNextra()
