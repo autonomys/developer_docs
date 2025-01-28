cacde9b7e14614346ff85310dc97322f061341c6
Papuang <github@charliemccombie.co.uk>
2025-01-28 00:25:31 +0000

    Update _meta.js

--- pages/evm/_meta.js
+++ pages/evm/_meta.js
@@ -1,8 +1,8 @@
 export default {
     introduction: 'Introduction',
-    general_information: 'General Information',
-    metamask: 'Setting Up MetaMask',
-    faucet: 'Faucet (get testnet tokens)',
+    general_information: 'Developer Tools',
+    metamask: 'Setting up MetaMask',
+    faucet: 'Faucet (get tAI3 testnet tokens)',
     block_explorer: 'Block Explorer',
     remix: 'Remix Guide',
     foundry: 'Foundry Guide',
