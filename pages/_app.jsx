7eb323f66b5ed9515fce8a818673568a81b032cd
Emil Fattakhov <emil.e.fattakhov@gmail.com>
2025-01-29 10:32:11 -0500

    Reorder css imports in app_jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,5 @@
-import '../styles/globals.css';
-import 'nextra-theme-docs/style.css';
+import 'nextra-theme-docs/style.css';  // Move Nextra theme first
+import '../styles/globals.css';        // Then your global styles to allow overrides
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
