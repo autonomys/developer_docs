34ddd3784ec7c96dc5a9204eb3bcc874964f5dd5
Emil Fattakhov <emil.e.fattakhov@gmail.com>
2025-02-03 12:30:15 -0500

    Add Google Analytics tag

--- theme.config.jsx
+++ theme.config.jsx
@@ -15,6 +15,15 @@ export default {
         data-modal-disclaimer="This is a custom LLM for Autonomys Network with access to all documentation, forum and discord threads. Since this is AI technology please use your best judgment."
         async
       />
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-N417BD8BLW"></script>
+      <script>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-N417BD8BLW');
+        `}
+      </script>
     </>
   ),
   project: {
