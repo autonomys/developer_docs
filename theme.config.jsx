1141bcaa72b1ea61ea7b81c1914f8f12081c725c
Jim Counter <jim@subspace.network>
2025-05-19 11:04:51 +0100

    Add favicon details to theme.

--- theme.config.jsx
+++ theme.config.jsx
@@ -26,6 +26,12 @@ export default {
           `
         }}
       />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon.png"
+      />
     </>
   ),
   project: {
