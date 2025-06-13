cd5a54aae02269393529195a97380d2c84b10c42
Emil Fattakhov <emil.e.fattakhov@gmail.com>
2025-06-13 10:38:25 -0400

    Improve readability of Auto Drive documentation, add information about API reference and improve Gateway docs

---
+++ pages/sdk/auto-drive/_meta.js
@@ -0,0 +1,8 @@
+export default {
+    'overview_setup': 'Overview & Setup',
+    'create_api_key': 'Create an API Key',
+    'available_functions': 'Available Functions',
+    'usage_examples': 'Usage Examples',
+    'gateway': "Gateway",
+    'api_reference': 'API Reference',
+}
