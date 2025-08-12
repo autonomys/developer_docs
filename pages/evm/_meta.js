33691c5cd4b20f9ba6d91fb522228a2328d65919
Emil Fattakhov <emil.e.fattakhov@gmail.com>
2025-08-12 15:14:57 -0400

    Update the page name for WAI3

--- pages/evm/_meta.js
+++ pages/evm/_meta.js
@@ -10,7 +10,7 @@ export default {
     hardhat: 'HardHat Guide',
     local: 'Local Development',
     the_graph: 'Query On-chain Data with The Graph',
-    wrapping_ai3: "Wrapping AI3 on Taurus",
+    wrapping_ai3: "Wrapping AI3 (WAI3)",
     bridge: "Bridge",
     NFT_guide: 'Minting and Sending NFTs',
   }
